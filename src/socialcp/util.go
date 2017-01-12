9684ba6b901d80082a85f88747e5466b66282219
Cristiano Matte <cristiano.matte@corp.globo.com>
2017-01-12 19:42:42 -0200

    Add util to handle configuration file

---
+++ src/socialcp/util.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Configuration struct {
+  Host string
+  User string
+  Slots map[int]string
+}
+
+var filePath = "configuration.json"
+var configuration *Configuration;
+
+func LoadConfiguration() {
+  data, error := ioutil.ReadFile(filePath)
+
+  if error != nil {
+    InitializeConfiguration()
+    return
+  }
+
+  error = json.Unmarshal(data, &configuration)
+
+  if error != nil {
+    InitializeConfiguration()
+    return
+  }
+}
+
+func SaveConfiguration() {
+  if configuration == nil {
+    InitializeConfiguration()
+  }
+
+  serializedConfiguration, error := json.Marshal(configuration)
+
+  if error != nil {
+    return
+  }
+
+  ioutil.WriteFile(filePath, serializedConfiguration, 0644)
+}
+
+func InitializeConfiguration() {
+  configuration = new(Configuration)
+  configuration.Host = "http://socialcp.cloud.dev.globoi.com/api/"
+  configuration.User = ""
+  configuration.Slots = make(map[int]string)
+}
+
+func SetHost(host string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  configuration.Host = host
+}
+
+func GetHost() (string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  return configuration.Host
+}
+
+func SetUser(user string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  configuration.User = user
+}
+
+func GetUser() (string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  return configuration.User
+}
+
+func SetRecipient(slot int, email string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  configuration.Slots[slot] = email
+}
+
+func GetRecipient(slot int) (string) {
+  if configuration == nil {
+    LoadConfiguration()
+  }
+
+  return configuration.Slots[slot]
+}
