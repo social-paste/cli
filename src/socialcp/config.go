4383ef094420c1223fa399f50d20599c21e0d492
Cristiano Matte <cristiano.matte@corp.globo.com>
2017-01-12 21:14:06 -0200

    feat: Implement config feature

--- src/socialcp/config.go
+++ src/socialcp/config.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 var cmdConfig = &Command{
 	Run:       runConfig,
 	UsageLine: "config ",
@@ -16,6 +22,33 @@ func init() {
 
 // runConfig executes config command and return exit code.
 func runConfig(args []string) int {
+	editor := "vi"
 
+	path, error := exec.LookPath(editor)
+	if error != nil {
+		fmt.Println("Erro ao localizar o editor de texto " + editor + ".")
+		fmt.Println(error)
+		return -1
+	}
+
+	cmd := exec.Command(path, FilePath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	error = cmd.Start()
+	if error != nil {
+		fmt.Println("Erro ao iniciar o editor de texto " + editor + ".")
+		fmt.Println(error)
+		return -1
+	}
+
+	error = cmd.Wait()
+	if error != nil {
+		fmt.Println(error)
+		return -1
+	}
+
+	fmt.Println("Configuração salva com sucesso!")
 	return 0
 }
