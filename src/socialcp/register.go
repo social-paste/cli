003cc5860b315d74ddde405595def518528146c5
Isadora Possebon <isadora.possebon@corp.globo.com>
2017-01-12 16:46:57 -0200

    feat: Implement register

--- src/socialcp/register.go
+++ src/socialcp/register.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"bytes"
+	"net/http"
+	"io/ioutil"
+)
 var cmdRegister = &Command{
 	Run:       runRegister,
 	UsageLine: "register ",
@@ -16,6 +22,32 @@ func init() {
 
 // runRegister executes register command and return exit code.
 func runRegister(args []string) int {
+	url := "http://socialcp.cloud.dev.globoi.com/api/accounts/"
 
-	return 0
+	json := []byte(fmt.Sprintf(`{"email":"%s"}`, args[0]))
+
+	request, error := http.NewRequest("POST", url, bytes.NewBuffer(json))
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+    panic(error)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == 201 {
+		fmt.Println("Usuário registrado com sucesso")
+		return 0
+	}
+
+	fmt.Println("Erro ao registrar usuário")
+	body, _ := ioutil.ReadAll(response.Body)
+
+	if string(body) != "" {
+		fmt.Println("Motivo: ", string(body))
+	}
+
+	return -1
 }
